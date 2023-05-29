b126889a119d29949381a3466ab85f940a619039
Aidar <65452919+Aidar05@users.noreply.github.com>
2023-05-29 17:41:43 +0600

    Update Drawing game.py

--- Drawing game.py
+++ Drawing game.py
@@ -45,67 +45,6 @@ rect_color = WHITE    # цвет кнопки по умолчанию
 y_start = display_height-(rect_height+margin)    # Начало диапозона         
 y_end = display_height-margin    # Конец диапозона
 
-# Двумерный список. В первом элементе находится диапозон в котором находиться кнопка по оси x. Второй элесент ось y
-rect_white_pos = [
-    [margin, rect_width+margin], # i*rect_width+(margin*(i+1)), (rect_width*(i+1))+(margin*(i+1))
-    [y_start, y_end]
-        ]
-
-rect_red_pos = [
-    [rect_width+(margin*2), 2*rect_width+(margin*2)],
-    [y_start, y_end]
-        ]
-
-rect_green_pos = [
-    [rect_width*2+(margin*3), rect_width*3+(margin*3)],
-    [y_start, y_end]
-        ]
-
-rect_blue_pos = [
-    [rect_width*3+(margin*4), rect_width*4+(margin*4)],
-    [y_start, y_end]
-        ]
-
-rect_yellow_pos = [
-    [rect_width*4+(margin*5), rect_width*5+(margin*5)],
-    [y_start, y_end]
-        ]
-
-rect_brown_pos = [
-    [rect_width*5+(margin*6), rect_width*6+(margin*6)],
-    [y_start, y_end]
-]
-
-rect_purple_pos = [
-    [rect_width*6+(margin*7), rect_width*7+(margin*6)],
-    [y_start, y_end]
-        ]
-
-rect_gray_pos = [
-    [rect_width*7+(margin*8), rect_width*8+(margin*7)],
-    [y_start, y_end]
-]
-
-rect_orange_pos = [
-    [rect_width*8+(margin*9), rect_width*9+(margin*8)],
-    [y_start, y_end]
-]
-
-rect_LightBlue_pos = [
-    [rect_width*9+(margin*10), rect_width*10+(margin*9)],
-    [y_start, y_end]
-]
-
-rect_pink_pos = [
-    [rect_width*10+(margin*11), rect_width*11+(margin*10)],
-    [y_start, y_end]
-]
-
-rect_black_pos = [
-    [rect_width*11+(margin*12), rect_width*12+(margin*11)],
-    [y_start, y_end]
-]
-
 
 # Матрица помагающая понять в какой цвет окрашивать определнный квадрат. Матрица заполнена нулями. Каждый элемент матрицы отвечает за какой-то квадрат на поле
 board = [[0]*square_count_horizontal for i in range(square_count_vertical)]
@@ -133,104 +72,14 @@ while True:
                 # Если он нажал на кнопку
                 if y_mouse_pos not in range(0, display_height-(margin+rect_height)):
 
-                    # Этот код работает если нажали на белую кнопку
-                    if x_mouse_pos in range(rect_white_pos[0][0], rect_white_pos[0][1]):
-                        if y_mouse_pos in range(rect_white_pos[1][0], rect_white_pos[1][1]):
-                            current_color = WHITE
-
-                    # Нажата красная кнопка
-                    elif x_mouse_pos in range(rect_red_pos[0][0], rect_red_pos[0][1]):
-                        if y_mouse_pos in range(rect_red_pos[1][0], rect_red_pos[1][1]):
-                            current_color = RED
-
-                    # Зеленая кнопка
-                    elif x_mouse_pos in range(rect_green_pos[0][0], rect_green_pos[0][1]):
-                        if y_mouse_pos in range(rect_green_pos[1][0], rect_green_pos[1][1]):
-                            current_color = GREEN
-
-                    # и т.д
-                    elif x_mouse_pos in range(rect_blue_pos[0][0], rect_blue_pos[0][1]):
-                        if y_mouse_pos in range(rect_blue_pos[1][0], rect_blue_pos[1][1]):
-                            current_color = BLUE
-
-                    elif x_mouse_pos in range(rect_yellow_pos[0][0], rect_yellow_pos[0][1]):
-                        if y_mouse_pos in range(rect_yellow_pos[1][0], rect_yellow_pos[1][1]):
-                            current_color = YELLOW
-                     
-                    elif x_mouse_pos in range(rect_brown_pos[0][0], rect_brown_pos[0][1]):
-                        if y_mouse_pos in range(rect_brown_pos[1][0], rect_brown_pos[1][1]):
-                            current_color = BROWN
-
-                    elif x_mouse_pos in range(rect_purple_pos[0][0], rect_purple_pos[0][1]):
-                        if y_mouse_pos in range(rect_purple_pos[1][0], rect_purple_pos[1][1]):
-                            current_color = PURPLE
-                            
-                    elif x_mouse_pos in range(rect_gray_pos[0][0], rect_gray_pos[0][1]):
-                        if y_mouse_pos in range(rect_gray_pos[1][0], rect_gray_pos[1][1]):
-                            current_color = GRAY
-
-                    elif x_mouse_pos in range(rect_orange_pos[0][0], rect_orange_pos[0][1]):
-                        if y_mouse_pos in range(rect_orange_pos[1][0], rect_orange_pos[1][1]):
-                            current_color = ORANGE
-
-                    elif x_mouse_pos in range(rect_LightBlue_pos[0][0], rect_LightBlue_pos[0][1]):
-                        if y_mouse_pos in range(rect_LightBlue_pos[1][0], rect_LightBlue_pos[1][1]):
-                            current_color = LIGHT_BLUE
-                             
-                    elif x_mouse_pos in range(rect_pink_pos[0][0], rect_pink_pos[0][1]):
-                        if y_mouse_pos in range(rect_pink_pos[1][0], rect_pink_pos[1][1]):
-                            current_color = PINK
-
-                    elif x_mouse_pos in range(rect_pink_pos[0][0], rect_black_pos[0][1]):
-                        if y_mouse_pos in range(rect_black_pos[1][0], rect_black_pos[1][1]):
-                            current_color = BLACK
-
-                    # На всякий случай.
-                    else:
-                        pass
+                    color_index = x_mouse_pos // rect_width
+                    current_color = colors[color_index]
                 
                 # Если пользователь нажал на какой-нибудь квадрат
                 elif y_mouse_pos in range(0, display_height - (margin + rect_height)):
                     try:
                         # Цифры в определенном месте матрицы обозначают цвет квадрата ка который они отвеают
-                        if current_color == WHITE:
-                            board[sqrt_mouse_row][sqrt_mouse_column] = 0    # 0 - Белый
-
-                        elif current_color == RED: 
-                            board[sqrt_mouse_row][sqrt_mouse_column] = 1    # 1 - Красный
-                     
-                        elif current_color == GREEN:
-                            board[sqrt_mouse_row][sqrt_mouse_column] = 2    # 2 - Зеленый
-
-                        elif current_color == BLUE:
-                            board[sqrt_mouse_row][sqrt_mouse_column] = 3    # И т.д
-
-                        elif current_color == YELLOW:
-                            board[sqrt_mouse_row][sqrt_mouse_column] = 4    
-
-                        elif current_color == BROWN:
-                            board[sqrt_mouse_row][sqrt_mouse_column] = 5   
-
-                        elif current_color == PURPLE:
-                            board[sqrt_mouse_row][sqrt_mouse_column] = 6    
-
-                        elif current_color == GRAY:
-                            board[sqrt_mouse_row][sqrt_mouse_column] = 7   
-
-                        elif current_color == ORANGE:
-                            board[sqrt_mouse_row][sqrt_mouse_column] = 8
-
-                        elif current_color == LIGHT_BLUE:
-                            board[sqrt_mouse_row][sqrt_mouse_column] = 9
-
-                        elif current_color == PINK:
-                            board[sqrt_mouse_row][sqrt_mouse_column] = 10
-
-                        elif current_color == BLACK:
-                            board[sqrt_mouse_row][sqrt_mouse_column] = 11
-                        else:
-                            pass
-
+                        board[sqrt_mouse_row][sqrt_mouse_column] = color_index
                     except IndexError:
                         pass
 
@@ -243,7 +92,6 @@ while True:
             # Смотрим какой значение в матрице, в месте которое отвечает за именно этот квадрат и в зависимости от этого меняем текущий цыет
             # Если 0(Как в самом начале везде) то белый
             
-            # Цвет текущего квадрата в связи с знаеченим в матрице
             square_color = colors[board[row][column]]
 
             # Рисование самого поля
